6bcace351a83ea646ac19a85af2d22eac045a801
Iman Akbar <iman.akbar@dwidasa.com>
2021-06-22 21:01:52 +0700

    geolocation trial 1

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,23 +1,39 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <h1>IMAN AKBAR</h1>
     </v-app-bar>
-  <v-row>
-    <v-col v-for="(data,i) in listData" :key='i'>
-      <v-card>
-        <v-card-title>{{data.title}}</v-card-title>
-        <v-card-text>
+    <v-card>
+      <v-card-title>Location</v-card-title>
+      <v-card-text>
+        <p>
+          {{ location.region }}
+          {{ location.postalCode }}
+          {{ location.country }}
+          {{ location.city }}
+        </p>
+      </v-card-text>
+    </v-card>
+    <v-row>
+      <v-col v-for="(domainsData, i) in domainsData" :key="i">
+        <v-card>
+          <v-card-title>{{ domainsData.data }}</v-card-title>
+          <!-- <v-card-text>
           <p>{{data.item}}</p>
+        </v-card-text> -->
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-card>
+        <v-card-title>dataAs</v-card-title>
+        <v-card-text>
+          <v-row> Name: {{ dataAs.name }} </v-row>
+          <v-row> route: {{ dataAs.route }} </v-row>
         </v-card-text>
       </v-card>
-    </v-col>
-  </v-row>
-   <v-btn
+    </v-row>
+    <!-- <v-btn
                     x-large
                     color="success"
                     outlined
@@ -33,9 +49,9 @@
                       
                       ref="fileinput"
                     ></b-form-file>
-                  </b-form-group>
+                  </b-form-group> -->
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
@@ -49,15 +65,74 @@ export default {
     //
     listData:[],
     param:{title:'',
-    foto:null,}
+    foto:null,},
+    dataAs:{},
+    domains:{},
+    domainsData:[],
+    location:{}
   }),
   mounted(){
-    axios.get(`URL`)
-    .then((res)=>{
-      this.listData= res.data
-    })
+    this.getData()
+    this.getLOcate()
+    this.getLocateByIP()
+    this.getVisit1()
   },
   methods: {
+    getLocateByIP(){
+      fetch('https://api.ipify.org?format=json')
+      .then(result => result.json())
+      .then(data => console.log(data.ip))
+    },
+    getLOcate(){
+      console.log("Cookies: " + navigator.cookieEnabled);
+console.log("Browser Language: " + navigator.browserLanguage);
+console.log("Language: " + navigator.language);
+console.log("Platform: " + navigator.platform);
+console.log("Connection Speed: " + navigator.connectionSpeed);
+console.log("User Agent: " + navigator.userAgent);
+console.log("Webdriver: " + navigator.webdriver);
+console.log("Geolocation: " , navigator.geolocation);
+    },
+    getvisitByNpm(){
+  //     this.$getLocation(options)
+  // .then(coordinates => {
+  //   console.log(coordinates);
+  // });
+
+    },
+    getVisit1(){
+    //   navigator.geolocation.getCurrentPosition(function(position){
+    //     console.log("longitude:"position.coords.longitude)
+    //     console.log("latitude:"position.coords.latitude)
+    // })
+    let check = (position) =>{
+      const{latitude,longitude} = position.coords;
+      fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=d8ed7c30fde54744bf25c812465d0115`)
+      .then(response => response.json())
+      .then(console.log)
+    }
+    navigator.geolocation.getCurrentPosition(check, console.log)
+    },
+    getData(){
+      axios.get(`https://geo.ipify.org/api/v1?apiKey=at_IRAUZj8fPx2gHBdlZfU6wt0AiuWpL&ipAddress=8.8.8.8`)
+    .then((res)=>{
+      console.log(res);
+      this.dataAs = res.data.as
+      this.domains = res.data.domains
+      this.location = res.data.location
+      console.log(this.dataAs);
+      console.log(this.domains);
+      for(let i = 0 ; i < this.domains.length ; i++){
+        this.domainsData.push({
+          data:this.domains[i]
+        })
+      }
+      console.log(this.domainsData);
+      console.log(this.location);
+      // this.listData= res.data
+    })
+    .catch((err)=>console.log(err))
+    },
     upload(event) {
       console.log("VALUE " + JSON.stringify(event));
       this.foto = event.target.files[0];
