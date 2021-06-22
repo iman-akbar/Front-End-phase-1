2ee8eb76b645d5cb78a92c9f2071ddf09b7117e9
Iman Akbar <iman.akbar@dwidasa.com>
2021-06-22 21:11:05 +0700

    visitor geo location finish

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -20,7 +20,7 @@
           <v-card-title>{{ domainsData.data }}</v-card-title>
           <!-- <v-card-text>
           <p>{{data.item}}</p>
-        </v-card-text> -->
+          </v-card-text>-->
         </v-card>
       </v-col>
     </v-row>
@@ -28,8 +28,8 @@
       <v-card>
         <v-card-title>dataAs</v-card-title>
         <v-card-text>
-          <v-row> Name: {{ dataAs.name }} </v-row>
-          <v-row> route: {{ dataAs.route }} </v-row>
+          <v-row>Name: {{ dataAs.name }}</v-row>
+          <v-row>route: {{ dataAs.route }}</v-row>
         </v-card-text>
       </v-card>
     </v-row>
@@ -49,7 +49,7 @@
                       
                       ref="fileinput"
                     ></b-form-file>
-                  </b-form-group> -->
+    </b-form-group>-->
     <v-main>
       <router-view />
     </v-main>
@@ -59,92 +59,129 @@
 <script>
 import axios from "axios";
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
     //
-    listData:[],
-    param:{title:'',
-    foto:null,},
-    dataAs:{},
-    domains:{},
-    domainsData:[],
-    location:{}
+    listData: [],
+    param: { title: "", foto: null },
+    dataAs: {},
+    domains: {},
+    domainsData: [],
+    location: {},
   }),
-  mounted(){
-    this.getData()
-    this.getLOcate()
-    this.getLocateByIP()
-    this.getVisit1()
+  mounted() {
+    this.getData();
+    this.getLOcate();
+    this.getLocateByIP();
+    this.getVisit1();
+    this.getvisitor();
   },
   methods: {
-    getLocateByIP(){
-      fetch('https://api.ipify.org?format=json')
-      .then(result => result.json())
-      .then(data => console.log(data.ip))
-    },
-    getLOcate(){
-      console.log("Cookies: " + navigator.cookieEnabled);
-console.log("Browser Language: " + navigator.browserLanguage);
-console.log("Language: " + navigator.language);
-console.log("Platform: " + navigator.platform);
-console.log("Connection Speed: " + navigator.connectionSpeed);
-console.log("User Agent: " + navigator.userAgent);
-console.log("Webdriver: " + navigator.webdriver);
-console.log("Geolocation: " , navigator.geolocation);
-    },
-    getvisitByNpm(){
-  //     this.$getLocation(options)
-  // .then(coordinates => {
-  //   console.log(coordinates);
-  // });
+    getvisitor() {
+      const countryCodeExpression = /loc=([\w]{2})/;
+      const userIPExpression = /ip=([\w.]+)/;
 
-    },
-    getVisit1(){
-    //   navigator.geolocation.getCurrentPosition(function(position){
-    //     console.log("longitude:"position.coords.longitude)
-    //     console.log("latitude:"position.coords.latitude)
-    // })
-    let check = (position) =>{
-      const{latitude,longitude} = position.coords;
-      fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=d8ed7c30fde54744bf25c812465d0115`)
-      .then(response => response.json())
-      .then(console.log)
-    }
-    navigator.geolocation.getCurrentPosition(check, console.log)
-    },
-    getData(){
-      axios.get(`https://geo.ipify.org/api/v1?apiKey=at_IRAUZj8fPx2gHBdlZfU6wt0AiuWpL&ipAddress=8.8.8.8`)
-    .then((res)=>{
-      console.log(res);
-      this.dataAs = res.data.as
-      this.domains = res.data.domains
-      this.location = res.data.location
-      console.log(this.dataAs);
-      console.log(this.domains);
-      for(let i = 0 ; i < this.domains.length ; i++){
-        this.domainsData.push({
-          data:this.domains[i]
-        })
+      function initCountry() {
+        return new Promise((resolve, reject) => {
+          var xhr = new XMLHttpRequest();
+          xhr.timeout = 3000;
+          xhr.onreadystatechange = function () {
+            if (this.readyState == 4) {
+              if (this.status == 200) {
+                let countryCode = countryCodeExpression.exec(this.responseText);
+                let ip = userIPExpression.exec(this.responseText);
+                if (
+                  countryCode === null ||
+                  countryCode[1] === "" ||
+                  ip === null ||
+                  ip[1] === ""
+                ) {
+                  reject("IP/Country code detection failed");
+                }
+                let result = {
+                  countryCode: countryCode[1],
+                  IP: ip[1],
+                };
+                resolve(result);
+              } else {
+                reject(xhr.status);
+              }
+            }
+          };
+          xhr.ontimeout = function () {
+            reject("timeout");
+          };
+          xhr.open("GET", "https://www.cloudflare.com/cdn-cgi/trace", true);
+          xhr.send();
+        });
       }
-      console.log(this.domainsData);
-      console.log(this.location);
-      // this.listData= res.data
-    })
-    .catch((err)=>console.log(err))
+      initCountry()
+        .then((result) => console.log(JSON.stringify(result)))
+        .catch((e) => console.log(e));
+    },
+    getLocateByIP() {
+      fetch("https://api.ipify.org?format=json")
+        .then((result) => result.json())
+        .then((data) => console.log(data.ip));
+    },
+    getLOcate() {
+      console.log("Cookies: " + navigator.cookieEnabled);
+      console.log("Browser Language: " + navigator.browserLanguage);
+      console.log("Language: " + navigator.language);
+      console.log("Platform: " + navigator.platform);
+      console.log("Connection Speed: " + navigator.connectionSpeed);
+      console.log("User Agent: " + navigator.userAgent);
+      console.log("Webdriver: " + navigator.webdriver);
+      console.log("Geolocation: ", navigator.geolocation);
+    },
+
+    getVisit1() {
+      let check = (position) => {
+        const { latitude, longitude } = position.coords;
+        fetch(
+          `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=d8ed7c30fde54744bf25c812465d0115`
+        )
+          .then((response) => response.json())
+          .then(console.log);
+      };
+      navigator.geolocation.getCurrentPosition(check, console.log);
+    },
+    getData() {
+      axios
+        .get(
+          `https://geo.ipify.org/api/v1?apiKey=at_IRAUZj8fPx2gHBdlZfU6wt0AiuWpL&ipAddress=8.8.8.8`
+        )
+        .then((res) => {
+          console.log(res);
+          this.dataAs = res.data.as;
+          this.domains = res.data.domains;
+          this.location = res.data.location;
+          console.log(this.dataAs);
+          console.log(this.domains);
+          for (let i = 0; i < this.domains.length; i++) {
+            this.domainsData.push({
+              data: this.domains[i],
+            });
+          }
+          console.log(this.domainsData);
+          console.log(this.location);
+          // this.listData= res.data
+        })
+        .catch((err) => console.log(err));
     },
     upload(event) {
       console.log("VALUE " + JSON.stringify(event));
       this.foto = event.target.files[0];
     },
-    newData(){
-      axios.post(`URL`, this.param)
-    .then((res)=>{
-      this.listData.push(res.data)
-
-    })
-    .catch((err)=>console.log(err))
-    }
+    newData() {
+      axios
+        .post(`URL`, this.param)
+        .then((res) => {
+          this.listData.push(res.data);
+        })
+        .catch((err) => console.log(err));
+    },
   },
 };
 </script>
